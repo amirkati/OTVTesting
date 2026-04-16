bef3d9a7757a10acbf720bac7e38d44965d60012
Katie A <katyask8s@gmail.com>
2026-04-16 12:13:11 -0400

    Added preliminary code for AMG8833 IR Array sensor Added the support for varying motor speeds via PWM (pulse width modulation)

--- OTVTesting.ino
+++ OTVTesting.ino
@@ -7,6 +7,14 @@
 
 #include "Enes100.h" // Includes the ENES100 library (which includes the VisionSystemClient.cpp)
 #include <SCServo.h>
+#include <Wire.h>
+#include <Adafruit_AMG88xx.h>
+
+// This sensor uses I2C to communicate. The device's I2C address is 0x69
+
+Adafruit_AMG88xx amg;
+
+float pixels[AMG88xx_PIXEL_ARRAY_SIZE];
 
 // Choose one: testboard or otv
 // #define HARDWARE_TESTBOARD
@@ -21,11 +29,11 @@
 // SWAP IN1 with IN2 (or IN3 with IN4) to reverse motor direction
 // If the thermal camera angle keeps spinning then SWAP pairs (IN1, IN2) with (IN3, IN4) to swap left and right motor
 #define DCM_IN1 7
-#define DCM_IN2 6
-#define DCM_IN3 5
-#define DCM_IN4 4
-#define DCM_ENA 9
-#define DCM_ENB 3
+#define DCM_IN2 6 
+#define DCM_IN3 5 
+#define DCM_IN4 4 
+#define DCM_ENA 12 
+#define DCM_ENB 13  
 
 /* This factor needs to be adjusted so that it gives number of seconds for motors to run to travel 1 meter */
 #define DCM_DISTANCE_TO_TIME_FACTOR       1.5
@@ -41,10 +49,10 @@
 // Students have had difficulty with the Mega. Some students have a certain set of pins work while other students will find those same pins broken.
 
 // Change these based on where the ESP8266 is actually connected. Check that it is an allowed pin for Mega.
-#define ESP8266_TX        10
-#define ESP8266_RX        11
+#define ESP8266_TX        14 // 10 
+#define ESP8266_RX        15 // 11
 // Ask TA and change ESP8266_MARKER
-#define ESP8266_MARKER    25
+#define ESP8266_MARKER    397
 #define ESP8266_ROOM      1116
 
 // change this if ARUCO 0 angle does not align with Y direction that points from landing zone to finish.
@@ -89,6 +97,9 @@
 #define DCM_DIR_FORWARD   0
 #define DCM_DIR_BACKWARD  1
 #define DCM_DIR_OFF       255
+#define DCM_MOTOR_SPEED_A 80
+#define DCM_MOTOR_SPEED_B 80
+// or do 60 
 
 // TODO is a #define - every time the compiler sees it, it substitutes the string to the right of TODO. It’s a convenient way to leave messages (from my point of view). 
 #define TODO(msg) fprintf(stderr, "*** TODO %s %s:%d\n", msg, __FILE__, __LINE__);
@@ -99,38 +110,39 @@
 // in1 determines voltage applied to wire 1 of motor
 // in2 determines voltage applied to wire 2 of motor
 // Arduino defines LOW as 0 and HIGH as 1
-// We may have to swap 'forward' and 'backward' labels depending on how the motors are wired to the H-bridge.  
-void set_dc_motor(int motor, int dir)
+// We may have to swap 'forward' and 'backward' labels depending on how the motors are wired to the H-bridge.
+// Speed is from 0 to 255  
+void set_dc_motor(int motor, int dir, int speed)
 {
-int en, in1, in2;
-if(dir==DCM_DIR_OFF) {
-    en=0;
-    in1=LOW;
-    in2=LOW;
-    } else 
-if(dir==DCM_DIR_FORWARD) {
-    en=255;
-    in1=HIGH;
-    in2=LOW;
-    } else
-if(dir==DCM_DIR_BACKWARD) {
-    en=255;
-    in1=LOW;
-    in2=HIGH;
-    }
+  int en, in1, in2;
+  if(dir==DCM_DIR_OFF) {
+      en=0;
+      in1=LOW;
+      in2=LOW;
+      } else 
+  if(dir==DCM_DIR_FORWARD) {
+      en=speed;
+      in1=HIGH;
+      in2=LOW;
+      } else
+  if(dir==DCM_DIR_BACKWARD) {
+      en=speed;
+      in1=LOW;
+      in2=HIGH;
+      }
 
 // analogWrite and digitalWrite are Arduino functions that set register. 'register' is a memory location that directly controls the hardware. (For example, input or output pins.)  
 
-if(motor==DCM_MOTOR1) {
-  analogWrite(DCM_ENA,   en);
-  digitalWrite(DCM_IN1,   in1);
-  digitalWrite(DCM_IN2,  in2);
-  }
-if(motor==DCM_MOTOR1) {
-  analogWrite(DCM_ENB, en); 
-  digitalWrite(DCM_IN3, in1);
-  digitalWrite(DCM_IN4, in2);
-  }
+  if(motor==DCM_MOTOR1) {
+    analogWrite(DCM_ENA,   en);
+    digitalWrite(DCM_IN1,   in1);
+    digitalWrite(DCM_IN2,  in2);
+    }
+  if(motor==DCM_MOTOR1) {
+    analogWrite(DCM_ENB, en); 
+    digitalWrite(DCM_IN3, in1);
+    digitalWrite(DCM_IN4, in2);
+    }
 }
 
 double current_x, current_y, current_angle;
@@ -141,21 +153,21 @@ double current_x, current_y, current_angle;
 // The negative distance means it moves backward.
 void move(double distance)
 {
-if(distance>0) {
-  set_dc_motor(DCM_MOTOR1, DCM_DIR_FORWARD);
-  set_dc_motor(DCM_MOTOR2, DCM_DIR_FORWARD);
-  } else 
-if(distance<0) {
-  set_dc_motor(DCM_MOTOR1, DCM_DIR_BACKWARD);
-  set_dc_motor(DCM_MOTOR2, DCM_DIR_BACKWARD);
-  }
+  if(distance>0) {
+    set_dc_motor(DCM_MOTOR1, DCM_DIR_FORWARD, DCM_MOTOR_SPEED_A);
+    set_dc_motor(DCM_MOTOR2, DCM_DIR_FORWARD, DCM_MOTOR_SPEED_B);
+    } else 
+  if(distance<0) {
+    set_dc_motor(DCM_MOTOR1, DCM_DIR_BACKWARD, DCM_MOTOR_SPEED_A);
+    set_dc_motor(DCM_MOTOR2, DCM_DIR_BACKWARD, DCM_MOTOR_SPEED_B);
+    }
 // The DC motors do not provide any feedback, so the distance it goes will have to be determined by running time. 
 // DCM_DISTANCE_TO_TIME_FACTOR is a constant that converts desired distance in meters to the time we'll need to run the motors for. 
 // fabs() is absolute value for floating point numbers (real numbers that the computer stores) (like double or float) 
-delay(fabs(distance)*DCM_DISTANCE_TO_TIME_FACTOR);
-set_dc_motor(DCM_MOTOR1, DCM_DIR_OFF);
-set_dc_motor(DCM_MOTOR2, DCM_DIR_OFF);
-TODO("set DCM_DISTANCE_TO_TIME_FACTOR to values measured on real robot")
+  delay(fabs(distance)*DCM_DISTANCE_TO_TIME_FACTOR);
+  set_dc_motor(DCM_MOTOR1, DCM_DIR_OFF, DCM_MOTOR_SPEED_A);
+  set_dc_motor(DCM_MOTOR2, DCM_DIR_OFF, DCM_MOTOR_SPEED_B);
+  TODO("set DCM_DISTANCE_TO_TIME_FACTOR to values measured on real robot")
 }
 
 // This function will set the motors to rotate in opposite direction, which allows the OTV robot to rotate. It tells the robot to rotate some number of 
@@ -163,20 +175,20 @@ TODO("set DCM_DISTANCE_TO_TIME_FACTOR to values measured on real robot")
 // It has its own ANGLE_TO_TIME constant (measured in degrees) but otherwise remains the same as the previous function. 
 void rotate_relative(double angle)
 {
-if(angle>0) {
-  set_dc_motor(DCM_MOTOR1, DCM_DIR_FORWARD);
-  set_dc_motor(DCM_MOTOR2, DCM_DIR_BACKWARD);
-  } else
-if(angle<0)
+  if(angle>0) {
+    set_dc_motor(DCM_MOTOR1, DCM_DIR_FORWARD, DCM_MOTOR_SPEED_A);
+    set_dc_motor(DCM_MOTOR2, DCM_DIR_BACKWARD, DCM_MOTOR_SPEED_B);
+    } else
+  if(angle<0)
   {
-  set_dc_motor(DCM_MOTOR1, DCM_DIR_BACKWARD);
-  set_dc_motor(DCM_MOTOR2, DCM_DIR_FORWARD);
+    set_dc_motor(DCM_MOTOR1, DCM_DIR_BACKWARD, DCM_MOTOR_SPEED_A);
+    set_dc_motor(DCM_MOTOR2, DCM_DIR_FORWARD, DCM_MOTOR_SPEED_B);
   }
 // DC motors do not provide any feedback, they go by running time
-delay(fabs(angle)*DCM_ANGLE_TO_TIME_FACTOR);
-set_dc_motor(DCM_MOTOR1, DCM_DIR_OFF);
-set_dc_motor(DCM_MOTOR2, DCM_DIR_OFF);
-TODO("set DCM_ANGLE_TO_TIME_FACTOR to value measured on real robot")
+  delay(fabs(angle)*DCM_ANGLE_TO_TIME_FACTOR);
+  set_dc_motor(DCM_MOTOR1, DCM_DIR_OFF, DCM_MOTOR_SPEED_A);
+  set_dc_motor(DCM_MOTOR2, DCM_DIR_OFF, DCM_MOTOR_SPEED_B);
+  TODO("set DCM_ANGLE_TO_TIME_FACTOR to value measured on real robot")
 }
 
 int ultra_trig[3]={ULTRA1_TRIG, ULTRA2_TRIG, ULTRA3_TRIG};
@@ -189,62 +201,64 @@ int ultra_echo[3]={ULTRA1_ECHO, ULTRA2_ECHO, ULTRA3_ECHO};
 // Returns distance in meters
 double read_ultrasonic_sensor(int sensor)
 {
-if(sensor<0)return(-1);
-if(sensor>2)return(-1);
+  if(sensor<0)return(-1);
+  if(sensor>2)return(-1);
 
-digitalWrite(ultra_trig[sensor], LOW);
-delayMicroseconds(2);
-digitalWrite(ultra_trig[sensor], HIGH);
-delayMicroseconds(10);
-digitalWrite(ultra_trig[sensor], LOW);
-double duration=pulseIn(ultra_echo[sensor], HIGH);
-return(duration*0.000343*0.5);
+  digitalWrite(ultra_trig[sensor], LOW);
+  delayMicroseconds(2);
+  digitalWrite(ultra_trig[sensor], HIGH);
+  delayMicroseconds(10);
+  digitalWrite(ultra_trig[sensor], LOW);
+  double duration=pulseIn(ultra_echo[sensor], HIGH);
+  return(duration*0.000343*0.5);
 }
 
 // To be coded later
 void extend_arm(int extend)
 {
-TODO("extend arm when extend is 1, retract when extend is 0")
+  TODO("extend arm when extend is 1, retract when extend is 0")
 }
+
+// IMU isn't used or is inside a part 
 
 double angle_to_gravity()
 {
-TODO("angle to gravity from IMU")
-return(0);
+  TODO("angle to gravity from IMU")
+  return(0);
 }
 
 double angle_to_north()
 {
-TODO("angle to north from IMU")
-return(0);
+  TODO("angle to north from IMU")
+  return(0);
 }
 
 double aruco_x()
 {
-#ifdef HARDWARE_OTV
-return(Enes100.getX());
-#endif
-return(0);
+  #ifdef HARDWARE_OTV
+  return(Enes100.getX());
+  #endif
+  return(0);
 }
 
 double aruco_y()
 {
-#ifdef HARDWARE_OTV
-return(Enes100.getY());
-#endif
-return(0);
+  #ifdef HARDWARE_OTV
+  return(Enes100.getY());
+  #endif
+  return(0);
 }
 
 // Assuming angle 0 is NORTH towards increasing y 
 double aruco_angle()
 {
-#ifdef HARDWARE_OTV
-double a=Enes100.getTheta()*180/3.1415-ARUCO_Y_ANGLE;
-if(a> 180)a-=360.0;
-if(a< -180)a+=360.0;
-return(a);
-#endif
-return(0);
+  #ifdef HARDWARE_OTV
+  double a=Enes100.getTheta()*180/3.1415-ARUCO_Y_ANGLE;
+  if(a> 180)a-=360.0;
+  if(a< -180)a+=360.0;
+  return(a);
+  #endif
+  return(0);
 }
 
 // This function will tell the motors to rotate with respect to absolute direction
@@ -273,39 +287,39 @@ SMS_STS st;
 
 void blanket_down(void)
 {
-st.WritePosEx(BLANKET_SERVO, BLANKET_DOWN_POSITION, 3400, 50);
+  st.WritePosEx(BLANKET_SERVO, BLANKET_DOWN_POSITION, 3400, 50);
 }
 
 void blanket_up(void)
 {
-st.WritePosEx(BLANKET_SERVO, BLANKET_UP_POSITION, 3400, 50);
+  st.WritePosEx(BLANKET_SERVO, BLANKET_UP_POSITION, 3400, 50);
 }
 
 // To be coded later
 int flame_detected()
 {
-TODO("This tells if it sees the flame")
-return(0);
+  TODO("This tells if it sees the flame")
+  return(0);
 }
 
 double angle_to_flame()
 {
-if(!flame_detected())return(-1e20);
-TODO("determine angle to flame from IR array")
-return(0);
+  if(!flame_detected())return(-1e20);
+  TODO("determine angle to flame from IR array")
+  return(0);
 }
 
 double forward_obstacle_distance()
 {
-TODO("Determine forward_obstacle distance from ultrasound or another sensor")
-return(0);
+  TODO("Determine forward_obstacle distance from ultrasound or another sensor")
+  return(0);
 }
 
 // This function will give the estimated distance to flame (in meters). 
 double distance_to_flame()
 {
-if(!flame_detected())return(-1e20);
-return(forward_obstacle_distance());
+  if(!flame_detected())return(-1e20);
+  return(forward_obstacle_distance());
 }
 
 // Change 50 to 100 or 150 if issues arise (higher number means less resolution and therefore less memory used)
@@ -324,58 +338,58 @@ int topo_map[NX][NY];
 // This function will initialize the map. 
 void init_map(void)
 {
-for(int i=0;i<NX;i++)
- for(int j=0;j<NY;j++) topo_map[i][j]=0;
+  for(int i=0;i<NX;i++)
+  for(int j=0;j<NY;j++) topo_map[i][j]=0;
 }
 
 // mark_map(20, 15, MARK_FIRE); An example
 void mark_map(double x, double y, int mark)
 {
-int x0=round(x/SQUARE_WIDTH_MM);
-int y0=round(y/SQUARE_WIDTH_MM);
+  int x0=round(x/SQUARE_WIDTH_MM);
+  int y0=round(y/SQUARE_WIDTH_MM);
 
-if((x0>=0) && (y0>=0) && (x0<NX) && (y0<NY))topo_map[x0][y0]=mark;
+  if((x0>=0) && (y0>=0) && (x0<NX) && (y0<NY))topo_map[x0][y0]=mark;
 }
 
 void traverse_to(double dest_x, double dest_y)
 {
-double dx, dy, angle;
-double tolerance=0.1;
-rotate_absolute(0);
+  double dx, dy, angle;
+  double tolerance=0.1;
+  rotate_absolute(0);
 
- TODO("Use aruco x,y and angle as well as map to drive to dest_x and dest_y. Update map as obstacles are found")
+  TODO("Use aruco x,y and angle as well as map to drive to dest_x and dest_y. Update map as obstacles are found")
 
-while(1) {
-  dx=dest_x-aruco_x();
-  dy=dest_y-aruco_y();
-  if(fabs(dx)<tolerance && fabs(dy)<tolerance)break;
-  angle=atan2(dy, dx)*180/3.1415;
-  rotate_absolute(angle);
-  move(0.05);
-  }
-rotate_absolute(0);
+  while(1) {
+    dx=dest_x-aruco_x();
+    dy=dest_y-aruco_y();
+    if(fabs(dx)<tolerance && fabs(dy)<tolerance)break;
+    angle=atan2(dy, dx)*180/3.1415;
+    rotate_absolute(angle);
+    move(0.05);
+    }
+  rotate_absolute(0);
 }
 
 void traverse_to_y(double dest_y)
 {
-TODO("Use aruco x,y and angle as well as map to drive to dest_y and some dest_x as happens. Update map as obstacles are found")
+  TODO("Use aruco x,y and angle as well as map to drive to dest_y and some dest_x as happens. Update map as obstacles are found")
 }
 
 void main_program(void)
 {
-current_x=aruco_x();
-current_y=aruco_y();
-current_angle=aruco_angle();
+  current_x=aruco_x();
+  current_y=aruco_y();
+  current_angle=aruco_angle();
 
-traverse_to(current_x, 150);
+  traverse_to(current_x, 150);
 
-// TODO("Find fire and face it")
+  // TODO("Find fire and face it")
 
-extend_arm(1);
-delay(10000); /* 10sec */
-extend_arm(0);
+  extend_arm(1);
+  delay(10000); /* 10sec */
+  extend_arm(0);
 
-traverse_to_y(3700);
+  traverse_to_y(3700);
 
 }
 
@@ -434,19 +448,39 @@ void setup()
   pinMode (ULTRA3_TRIG, OUTPUT);
   pinMode (ULTRA3_ECHO , INPUT);
 
-// Change 0 to 1 to make motors constantly spin forward for FORWARD LOCOMOTION to begin subtask 2
-/* 0 comments out the code essentially */ 
-#if 1
-  set_dc_motor(DCM_MOTOR1, DCM_DIR_FORWARD);
-  set_dc_motor(DCM_MOTOR2, DCM_DIR_FORWARD);
-  while(1); /* endless loop */
-#endif
+  // AMG8833 IR Array code
 
-// Change to 1 to begin subtask 3 when wifi is not working
-// Does a 90 degree turn and waits for 5 seconds. OTV performs this 3 times.
-// Depends on calibration factors to be correct. 
-// If Wifi works, then the code below this should work better
-#if 0
+  Serial.begin(9600);
+  Serial.println(F("AMG8833 pixels"));
+
+  bool status;
+    
+  // default settings
+  status = amg.begin();
+  if (!status) {
+       Serial.println("Could not find a valid AMG88xx sensor, check wiring!");
+       while (1);
+  }
+    
+  Serial.println("-- Pixels Test --");
+
+  Serial.println();
+
+  delay(100); // let sensor boot up
+
+  // Change 0 to 1 to make motors constantly spin forward for FORWARD LOCOMOTION to begin subtask 2
+  /* 0 comments out the code essentially */ 
+  #if 1
+  set_dc_motor(DCM_MOTOR1, DCM_DIR_FORWARD, DCM_MOTOR_SPEED_A);
+  set_dc_motor(DCM_MOTOR2, DCM_DIR_FORWARD, DCM_MOTOR_SPEED_B);
+  while(1); /* endless loop */
+  #endif
+  
+  // Change to 1 to begin subtask 3 when wifi is not working
+  // Does a 90 degree turn and waits for 5 seconds. OTV performs this 3 times.
+  // Depends on calibration factors to be correct. 
+  // If Wifi works, then the code below this should work better
+  #if 0
   /* 90 degree turns using rotate_relative(), without Aruco */
    // Rotate left first
   rotate_relative(90);
@@ -458,22 +492,22 @@ void setup()
   rotate_relative(90);
   while(1);
 
-#endif
+  #endif
 
-// Change 0 to 1 to begin subtask 10 if servo pins are connected (and written correctly) 
-// This elevates the blanket up and waits for 5 seconds, and then puts blanket down and waits for 5 seconds. It repeats. 
-// SERIAL_8N1 is a parity setting that describes the format of transmitted data. (The parity bit is 8 total data bits, no parity bit and one stop bit.)
-#if 0
-SERVO_SERIAL.begin(1000000, SERIAL_8N1);
-st.pSerial=&SERVO_SERIAL;
+  // Change 0 to 1 to begin subtask 10 if servo pins are connected (and written correctly) 
+  // This elevates the blanket up and waits for 5 seconds, and then puts blanket down and waits for 5 seconds. It repeats. 
+  // SERIAL_8N1 is a parity setting that describes the format of transmitted data. (The parity bit is 8 total data bits, no parity bit and one stop bit.)
+  #if 0
+  SERVO_SERIAL.begin(1000000, SERIAL_8N1);
+  st.pSerial=&SERVO_SERIAL;
 
-while(1) {
+  while(1) {
     blanket_up();
     delay(5000);
     blanket_down();
     delay(5000);
-}
-#endif
+  }
+  #endif
 
 
   // Enes100.begin(const char* teamName, byte teamType, int markerId, int roomNumber, int wifiModuleTX, int wifiModuleRX);
@@ -515,10 +549,10 @@ while(1) {
   
   #endif 
 
-// Change to 1 to begin subtask 3.
-// Does a 90 degree turn and waits for 5 seconds. OTV performs this 3 times.
-#if 0
-/* 90 degree rotation test */
+  // Change to 1 to begin subtask 3.
+  // Does a 90 degree turn and waits for 5 seconds. OTV performs this 3 times.
+  #if 0
+  /* 90 degree rotation test */
   rotate_absolute(0);
   delay(5000);
   rotate_absolute(90);
@@ -530,21 +564,21 @@ while(1) {
   rotate_absolute(0);
   while(1); /* endless loop */
   
-#endif
+  #endif
 
-// Change to 1 to begin subtask 6. 
-// Determines if it is in landing spot A or B and then goes to the opposite spot. 
-// Waits 5 seconds and goes to specified spot (before log) and stops movement. 
-#if 0
+  // Change to 1 to begin subtask 6. 
+  // Determines if it is in landing spot A or B and then goes to the opposite spot. 
+  // Waits 5 seconds and goes to specified spot (before log) and stops movement. 
+  #if 0
   /* Random mission test */
- if(aruco_x()<1.0)traverse_to(1.5, 0.4);
+  if(aruco_x()<1.0)traverse_to(1.5, 0.4);
      else traverse_to(0.5, 0.4);
-   delay(5000); /* wait 5 sec */
-   /* go to before log */
-   traverse_to(1.0, 2.1); 
-  while(1); /* endless loop */
-
-#endif
+     delay(5000); /* wait 5 sec */
+     /* go to before log */
+    traverse_to(1.0, 2.1); 
+    while(1); /* endless loop */
+    
+    #endif
 }
 
 
@@ -561,10 +595,27 @@ while(1) {
 int state=STATE_START; 
 
 // IF this compiles well with Mega and ESP8266 WiFi module, this code will tell the OTV to drive to the other end of the field (as wanted in the MS5 document)
+// This won't start as long as the void setup() is running.
 void loop() 
 {
   if(state==STATE_START) 
   {
+    // For the AMG8833 camera 
+    //read all the pixels
+    amg.readPixels(pixels);
+
+    Serial.print("[");
+    for(int i=1; i<=AMG88xx_PIXEL_ARRAY_SIZE; i++){
+      Serial.print(pixels[i-1]);
+      Serial.print(", ");
+      if( i%8 == 0 ) Serial.println();
+      }
+    Serial.println("]");
+    Serial.println();
+
+    //delay a second
+    delay(1000);
+    
     rotate_absolute(0); 
     move(0.05); // An example 
     rotate_absolute(90); // It points towards the candles 
